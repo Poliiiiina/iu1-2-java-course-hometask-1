cce1bfbb6be49c241c178184cf273a4a7cc87d32
Poliiiiina <107002921+Poliiiiina@users.noreply.github.com>
2022-06-09 22:42:50 +0300

    Update Vehicle.java

--- src/classes/Vehicle.java
+++ src/classes/Vehicle.java
@@ -1,5 +1,25 @@
 package classes;
 
+
 public class Vehicle {
+    public String model;
+
+    public int averageVelocity;
+
+    public int fuelConsumption;
+
+    public Vehicle(String model, int averageVelocity, int fuelConsumption) {
+        this.model = model;
+        this.averageVelocity = averageVelocity;
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getTimeOfDistance(double distance) {
+        return distance / averageVelocity;
+    }
+
+    public double getFuelAmountForDistance(double distance) {
+        return fuelConsumption * distance / 100;
+    }
 
 }
