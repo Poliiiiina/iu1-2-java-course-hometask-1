a811ca9fd6caede7b1b5fbf677c5fa334ede8f5a
Poliiiiina <alejnikova.polina@bk.ru>
2022-06-09 22:26:58 +0300

    done

--- src/functions/Function1.java
+++ src/functions/Function1.java
@@ -3,8 +3,7 @@ package functions;
 public class Function1 {
     public static void main(String[] args) {
         System.out.println("Function testing:");
-        System.out.printf("Test 1 %s.\n", ClassForTest
-                .compareResults(findMax(new int[]{3, 4, 10, -1}), 10) ? "passed" : "failed");
+        System.out.printf("Test 1 %s.\n", ClassForTest.compareResults(findMax(new int[]{3, 4, 10, -1}), 10) ? "passed" : "failed");
 
         try {
             findMax(new int[0]);
@@ -18,6 +17,16 @@ public class Function1 {
     // обработать случай, когда длина массива равна нулю
     // в этом случае выбрасывать исключение
     private static int findMax(int[] arr) {
-        return 1;
+        int max_num = -1000000;
+        if (arr.length == 0){
+            throw new NullPointerException("Длина массива равна нулю");
+        } else {
+            for (int i = 0; i < arr.length; i++) {
+                if (max_num <arr[i]){
+                    max_num = arr[i];
+                }
+            }
+        }
+        return max_num;
     }
 }
--- src/functions/Function2.java
+++ src/functions/Function2.java
@@ -1,5 +1,7 @@
 package functions;
 
+import java.util.Arrays;
+
 public class Function2 {
     public static void main(String[] args) {
         int[] a = new int[]{1, 2, 3, 4, 5};
@@ -20,6 +22,21 @@ public class Function2 {
     которая объединит их и вернет упорядоченный массив с порядком таким же как и a
      */
     private static int[] merge(int[] a, int[] b) {
-        return new int[]{1};
+        int[] c = new int[a.length + b.length];
+        for (int i = 0; i < a.length; i++) {
+            c[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            c[i + a.length] = b[i];
+        }
+        Arrays.sort(c);
+        if (a.length > 1 && a[0] > a[1]) {
+            for (int i = 0; i < c.length / 2; i++) {
+                int temp = c[i];
+                c[i] = c[c.length - 1 - i];
+                c[c.length - 1 - i] = temp;
+            }
+        }
+        return c;
     }
 }
